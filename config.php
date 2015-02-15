b1a43917a79274396642a59f8369a3aa1f6266f7
tooltonix <patrick@tubenschleim.de>
2015-02-15 13:31:02 +0100

    add phpmailer config

--- config.php
+++ config.php
@@ -3,11 +3,20 @@
  * main config
  */
 $config = [
-	'debug' => true,
-	'data_dir' => dirname(__FILE__) . "/data/",
-	'data_files' => [
+	"debug" => true,
+	"data_dir" => dirname(__FILE__) . "/data/",
+	"data_files" => [
 		"profile" => "profile.json",
 		"skills" => "skills.json",
+	],
+	"smtp_config" => [
+		"host" => "xxx",
+		"username" => "xxx",
+		"password" => "xxx",
+		"port" => 587,
+		"secure" => "TLS",
+		"smtp_auth" => true,
+		"receiver" => ["mail" => "xxx", "name" => "xxx"],
 	],
 ];
 
@@ -15,7 +24,14 @@ $config = [
  * config for JSON-Middleware
  */
 $json_config = [
-	'json.status' => true,
-	'json.override_error' => true,
-	'json.override_notfound' => true,
+	"json.status" => true,
+	"json.override_error" => true,
+	"json.override_notfound" => true,
 ];
+
+/**
+ * include customized config file
+ */
+if (file_exists(dirname(__FILE__) . "/my_config.php")) {
+	include dirname(__FILE__) . "/my_config.php";
+}
